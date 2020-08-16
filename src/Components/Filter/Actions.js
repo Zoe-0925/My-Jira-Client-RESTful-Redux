18468f243d4eb2abbff99d0d74e4feb2fbb69e0a
Zoe Zhang <jin0925aki@gmail.com>
2020-08-16 10:07:52 +1000

    Fixed Groupby Select

--- src/Components/Filter/Actions.js
+++ src/Components/Filter/Actions.js
@@ -2,10 +2,7 @@ export const FILTER_BY_EPIC = "FILTER_BY_EPIC"
 export const FILTER_BY_LABEL = "FILTER_BY_LABEL"
 export const FILTER_BY_ASSIGNEE = "FILTER_BY_ASSIGNEE"
 export const CLEAN_FILTER = "CLEAN_FILTER"
-export const GROUP_BY_ASSIGNEE= "GROUP_BY_ASSIGNEE"
-export const GROUP_BY_EPIC= "GROUP_BY_EPIC"
-export const GROUP_BY_SUBTASK= "GROUP_BY_SUBTASK"
-
+export const GROUP_BY= "GROUP_BY"
 
 export const filterByEpic = (data) => {
     return {
